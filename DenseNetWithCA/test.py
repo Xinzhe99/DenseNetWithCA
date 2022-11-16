8126d7670cfe405e019203cb7e2ae83d6a15884e
XinzheXie <113503163+Xinzhe99@users.noreply.github.com>
2022-11-16 20:27:20 +0800

    Delete test.py

--- DenseNetWithCA/test.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import cv2
-import torchvision.transforms as transforms
-trans = transforms.Compose([transforms.ToTensor()])
-img = Image.open('00023.jpg').convert('L')
-img = trans(img)
-print(img.shape)
